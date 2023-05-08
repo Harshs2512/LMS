6c66039bc0fa0309fc56d8c232206742a01bdbb6
Harshs2512 <harshsoni1234ji@gmail.com>
2023-05-08 16:35:28 +0530

    harsh

--- backend/controllers/courseControllers.js
+++ backend/controllers/courseControllers.js
@@ -1,6 +1,6 @@
 const Course = require("../models/courseModel");
 
-
+// For admin
 exports.createCourse = async (req, res, next) => {
     const course = await Course.create(req.body);
     res.status(201).json({
@@ -9,6 +9,32 @@ exports.createCourse = async (req, res, next) => {
     })
 }
 
-exports.getAllCourses = (req, res) => {
-    res.status(200).json({ massage: "Route is working " })
+
+// Get all data
+exports.getAllCourses = async (req, res) => {
+    const courses = await Course.find();
+    res.status(200).json({
+        success: true,
+        courses
+    })
+}
+
+// Update Course
+exports.updateCourse = async (req, res, next) => {
+
+    var course = await Course.findById(req.params.id);
+    console.log(course)
+
+    if (!course) {
+        return res.status(500).json({
+            success: false,
+            message: "Course not found"
+        })
+    }
+    course = await Course.findByIdAndUpdate(req.params.id, res.body, { new: true, runValidators: true, useFindAndModify: false });
+    res.status(200).json({
+        success: true,
+        course,  
+    })
+    console.log(course)
 }
