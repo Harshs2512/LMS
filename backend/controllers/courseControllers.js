8ad835d2ad080b6221b632a54cfebf7cc1c29041
suresh kumar singh <s.k.singh72718@gmail.com>
2023-05-12 11:46:16 +0530

    SURESH

--- backend/controllers/courseControllers.js
+++ backend/controllers/courseControllers.js
@@ -1,75 +1,107 @@
 const Course = require("../models/courseModel");
+const Category = require("../models/categoryModel");
 const { param } = require("../routes/courseRoute");
 // const del = require("sample_mflix")
 
-
-// For admin
+//For admin
 exports.createCourse = async (req, res, next) => {
-    const course = await Course.create(req.body);
-    res.status(201).json({
-        success: true,
-        course
-    })
-}
-
+  const course = await Course.create(req.body);
+  res.status(201).json({
+    success: true,
+    course,
+  });
+};
 
 // Get all data
 exports.getAllCourses = async (req, res) => {
-    const courses = await Course.find();
-    res.status(200).json({
-        success: true,
-        courses
-    })
-}
+  const courses = await Course.find();
+  res.status(200).json({
+    success: true,
+    courses,
+  });
+};
 // Update Course
 exports.updateCourse = async (req, res) => {
+  let course = await Course.updateOne(
+    {
+      _id: req.params.id,
+    },
+    {
+      $set: req.body,
+    }
+  );
+  res.send(course);
 
-    let course = await Course.updateOne(
-        {
-            _id: req.params.id
-        },
-        {
-            $set: req.body
-        }
-    )
-    res.send(course)
-
-    // if (!course) {
-    //     return res.status(500).json({
-    //         success: false,
-    //         message: "Course not found"
-    //     })
-    // }
+  // if (!course) {
+  //     return res.status(500).json({
+  //         success: false,
+  //         message: "Course not found"
+  //     })
+  // }
 };
 //delete code
 exports.deleteCourse = async (req, res) => {
-    const result = await Course.deleteOne(
-        {
-            _id: req.params.id
-        })
-    }
-    // delete Course
-    // await Course.remove();
-    console.log(course)
+  const result = await Course.deleteOne({
+    _id: req.params.id,
+  });
 
-
-    //get All Course Details
-exports.getCourseDetails = async (req, res,next) => {
-    const result = await Course.findById(
-        {
-            _id: req.params.id
-        });
-        
-        // if (!result) {
-        //         return res.status(500).json({
-        //             success: false,
-        //             message: "Course not found"
-        //         })
-        //     }
-        //     res.status(200).json({
-        //         success: true,
-        //         result
-        //     })
-        //     res.send(result)
+  // delete Course
+  // await Course.remove();
+  console.log(result);
 };
 
+//get All Course Details
+exports.getCourseDetails = async (req, res, next) => {
+  const result = await Course.findById({
+    _id: req.params.id,
+  });
+
+  // if (!result) {
+  //         return res.status(500).json({
+  //             success: false,
+  //             message: "Course not found"
+  //         })
+  //     }
+  //     res.status(200).json({
+  //         success: true,
+  //         result
+  //     })
+  //     res.send(result)
+};
+//for add category
+
+exports.createCategory = async (req, res, next) => {
+  const category = await Category.create(req.body);
+  res.status(201).json({
+    success: true,
+    category,
+  });
+};
+// Gshow all  cetegory name
+exports.getAllCategory = async (req, res) => {
+  const category = await Category.find();
+  res.status(200).json({
+    success: true,
+    category,
+  });
+};
+
+// Update Category
+exports.updateCategory = async (req, res) => {
+  let category = await Category.updateOne(
+    {
+      _id: req.params.id,
+    },
+    {
+      $set: req.body,
+    }
+  );
+  res.send(category);
+};
+//delete  category
+exports.deleteCategory = async (req, res) => {
+  const result = await Category.deleteOne({
+    _id: req.params.id,
+  });
+  res.send(result); 
+};
--- backend/routes/courseRoute.js
+++ backend/routes/courseRoute.js
@@ -1,11 +1,17 @@
 const express = require('express');
-const { getAllCourses, createCourse, updateCourse, deleteCourse,getCourseDetails } = require("../controllers/courseControllers");
+const { getAllCourses, createCourse, updateCourse, deleteCourse,getCourseDetails,createCategory,getAllCategory,updateCategory,deleteCategory } = require("../controllers/courseControllers");
 
 const router = express.Router();
 
 
-router.route("/courses").get(getAllCourses);
+//For add course CRUD operation
 router.route("/course/new").post(createCourse);
-router.route("/course/:id").put(updateCourse).delete(deleteCourse).get(getCourseDetails)
+router.route("/courses").get(getAllCourses);
+router.route("/course/:id").put(updateCourse).delete(deleteCourse).get(getCourseDetails);
+
+//For add category CRUD operation
+router.route("/category").post(createCategory);
+router.route("/show_category").get(getAllCategory);
+router.route("/categry/:id").put(updateCategory).delete(deleteCategory);
 
 module.exports = router
