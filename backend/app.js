16ebc366e13a1854b3359a00e543389abf672f5f
Mohammad-Adnan-Ahmad <120508835+Mohammad-Adnan-Ahmad@users.noreply.github.com>
2023-05-18 13:20:46 +0530

    Update app.js
    some changes in app.js for user

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,19 @@
 const express = require("express");
 require("dotenv").config();
 require("./db");
+const user = require("./routes/userRoute");
 const admin =require("./routes/adminRoute")
 const errorMiddleware = require("./middleware/error")
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 
 const courseRoute = require("./routes/courseRoute");
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/api/v1", user);
 app.use("/api/v1", courseRoute);
 app.use("/api/v1", admin);
 
@@ -15,5 +21,5 @@ app.use("/api/v1", admin);
 app.use("/api/v1",courseRoute);
 
 app.listen(8000, () => {
-    console.log("the port is listening on port 8000");
+    console.log("the server is listening on port 8000");
 });
